75b5471e827328a1527e730352049b329abdb7ff
Kejanu <kejanuprojects@gmail.com>
2022-07-17 23:33:21 +0200

    Added equals based on id

--- src/main/java/de/kejanu/model/account/DbAccount.java
+++ src/main/java/de/kejanu/model/account/DbAccount.java
@@ -3,7 +3,7 @@ package de.kejanu.model.account;
 import de.kejanu.model.run.DbRun;
 
 import javax.persistence.*;
-import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -19,11 +19,24 @@ public class DbAccount {
 
     @ManyToMany
     @JoinTable(
-            name = "run_account",
-            joinColumns = {@JoinColumn( name = "account" )},
-            inverseJoinColumns = {@JoinColumn( name = "run" )}
+        name = "run_account",
+        joinColumns = {@JoinColumn( name = "account" )},
+        inverseJoinColumns = {@JoinColumn( name = "run" )}
     )
     private Set<DbRun> runs;
+
+    @Override
+    public boolean equals(Object o) {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+        DbAccount dbAccount = ( DbAccount ) o;
+        return Objects.equals(id, dbAccount.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
     public String getName() {
         return name;
